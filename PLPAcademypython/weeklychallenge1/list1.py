59bf5dbe51060159802dd0f4d6ef48aac639b2e4
Rowi-1 <rowi_1one@students.uonbi.ac.ke>
2024-03-11 10:16:59 +0300

    more on python

--- PLPAcademypython/weeklychallenge1/list1.py
+++ PLPAcademypython/weeklychallenge1/list1.py
@@ -1,1 +1,11 @@
-number = []
+# Accept user input to create a list of integers
+numbers = []
+while True:
+    num = input("Enter an integer (or 'done' to finish): ")
+    if num == 'done':
+        break
+    numbers.append(int(num))
+
+# Compute the sum of all integers in the list
+total = sum(numbers)
+print("Sum of the integers:", total)
---
+++ PLPAcademypython/weeklychallenge1/touple1.py
@@ -0,0 +1,5 @@
+# Create a tuple of book names
+favorite_books = ('Book 1', 'Book 2', 'Book 3', 'Book 4', 'Book 5')
+# Use a for loop to print each book name on a separate line
+for book in favorite_books:
+    print(book)
